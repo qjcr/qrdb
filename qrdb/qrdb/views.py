85c082de379cb6c80d6d9e8b7d36b5ef097d59c4
Mikel Bober-Irizar <mikel@mxbi.net>
2021-03-31 17:59:19 +0100

    Add map

--- qrdb/qrdb/views.py
+++ qrdb/qrdb/views.py
@@ -1,8 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from . import models
+
 def index(request):
     return render(request, "test.html")
 
 def view_map(request):
-    return render(request, "test.html")
+    feed_dict = {}
+    feed_dict['buildings'] = models.Building.objects.all()
+    return render(request, "test.html", feed_dict)
+
