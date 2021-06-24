386704c300851a75c859e7925bb12dce0b6131bc
Queens' College JCR <qjcr@sinkhole.srcf.net>
2021-06-24 19:18:18 +0100

    Update gitignore

--- qrdb/runserver.sh
+++ qrdb/runserver.sh
@@ -1,4 +1,6 @@
+#!/bin/bash -e
 pyenv global 3.9.5
 . ~/rooms/venv/bin/activate
-pyenv exec python manage.py runserver
+pyenv exec python manage.py migrate
+yes yes | pyenv exec python manage.py collectstatic
 pyenv exec gunicorn qrdb.wsgi -b unix:/societies/qjcr/rooms/web.sock #0.0.0.0:6969
